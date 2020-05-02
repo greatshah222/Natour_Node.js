a6356f9ea8d292d331d0881c039f9f9e5abb9b77
greatshah222 <great.shah222@gmail.com>
2020-05-02 13:14:02 +0300

    configuration of mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,96 +1,88 @@
 const fs = require('fs');
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-// this is param middleware  
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+// this is param middleware
 exports.checkID = (req, res, next, val) => {
-    console.log(`tour id is ${val}`)
-    if (req.params.id * 1 > tours.length) {
-        return res.status(401).json({
-            status: 'fail',
-            message: 'invalid TOur id'
-        })
-
-    }
-    next();
-}
+  console.log(`tour id is ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(401).json({
+      status: 'fail',
+      message: 'invalid TOur id',
+    });
+  }
+  next();
+};
 // this middleware is for checkinf isf the response body has both name and price in our response
-exports.checkBody = ((req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Missing either name or price.'
-        })
-    }
-    next();
-
-})
-
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Missing either name or price.',
+    });
+  }
+  next();
+};
 
 exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
-    res.status(200).json({
-        status: 'sucess',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: {
-            tours: tours
-        }
-
-    })
-}
+  console.log(req.requestTime);
+  res.status(200).json({
+    status: 'sucess',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+};
 
 exports.getTour = (req, res) => {
-    // we can get our parameter of request id using req.params
-    console.log(req.params)
-    // convert string to num
-    const id = req.params.id * 1;
+  // we can get our parameter of request id using req.params
+  console.log(req.params);
+  // convert string to num
+  const id = req.params.id * 1;
 
-    const tour = tours.find(el => el.id === id);
-    res.status(200).json({
-        status: 'success',
-        data:
-        {
-            tour: tour
-        }
-
-    });
-}
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour,
+    },
+  });
+};
 exports.createTour = (req, res) => {
-    // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    // assign is used to create new object by merging with another
-    const newTour = Object.assign({
-        id: newId
-    }, req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-
-    });
-
-
-}
-exports.updateTour = (req, res) => {
-    {
-        res.status(200).json({
-            status: 'succees',
-            data: {
-                tour: 'updates Tour'
-            }
-        })
-
-    }
-
-}
-exports.deleteTour = ((req, res) => {
-
-    res.status(204).json({
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  // assign is used to create new object by merging with another
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
         status: 'success',
-        data: null
-    })
-});
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+exports.updateTour = (req, res) => {
+  {
+    res.status(200).json({
+      status: 'succees',
+      data: {
+        tour: 'updates Tour',
+      },
+    });
+  }
+};
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
--- server.js
+++ server.js
@@ -1,13 +1,63 @@
 // Server
+const mongoose = require('mongoose');
+// thi smanggose is for connecting our application with the mangodb server in atlas
 // this dotenv is for config.env file to read variable from file and store in env variable of node.js
 const dotenv = require('dotenv');
 // we need to run dotnenv before we run in app file because all goes step by step
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // console.log(con.connections);
+    // put con in then to get con.connections result
+    console.log('db connected');
+  });
+// here in the required section the required is set to true if it is false it will return the erro ras specified in the second parameter
+// the default means it will be set to 4.5 if not specified
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'a tour must have a price'],
+  },
+});
 // console.log(process.env.NODE_ENV)
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
-module.exports = process.env.PORT || port;
+// uppercase on modelname and variable
+const Tour = mongoose.model('Tour', tourSchema);
+// testTour is a documented created out of tour model and is an instance of tour model
+const testTour = new Tour({
+  name: 'The RUnnign Hell',
+  rating: 4.7,
+  price: 497,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('error 🦺', err);
+  });
